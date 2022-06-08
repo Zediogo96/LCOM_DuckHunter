17aa74b1655233b27390968ec776b156fdd0ac51
Zé Diogo <up202003529@fe.up.pt>
2022-06-08 15:50:07 +0100

    Fixed a bug where crosshair was being displayed weirdly when touching with screen borders

--- proj/src/crosshair.c
+++ proj/src/crosshair.c
@@ -3,8 +3,9 @@
 void update_mouse(struct packet pp, Database *db) {
 
   if (pp.delta_x != 0 || pp.delta_y != 0) {
-    db->sprites->crosshair->x += pp.delta_x;
-    db->sprites->crosshair->y -= pp.delta_y;
+
+    if (db->sprites->crosshair->x + pp.delta_x > 0 && ((db->sprites->crosshair->x + db->sprites->crosshair->width) + pp.delta_x  <= get_h_res())) db->sprites->crosshair->x += pp.delta_x;
+    if (db->sprites->crosshair->y - pp.delta_y > 0 && ((db->sprites->crosshair->y + db->sprites->crosshair->height)  - pp.delta_y <= get_v_res())) db->sprites->crosshair->y -= pp.delta_y;
   }
 
   if (pp.lb) {
